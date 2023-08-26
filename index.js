8a4b910af288fd317608ef8b069b0ff729b55a38
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-08-26 13:21:34 +0300

    changed index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ dotenv.config();
 const app = express();
 const port =  process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello there!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
