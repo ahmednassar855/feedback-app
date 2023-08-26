a9c33a28dff3331295ba554929082084ccba5ad7
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-08-26 13:34:58 +0300

    changed port in both index and env file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port =  process.env.PORT || 5000;
+const port =  process.env.PORT || 5001;
 
 app.get('/', (req, res) => res.send('Hello there!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
