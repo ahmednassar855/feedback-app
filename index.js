0632927029b6e3df151d89b6f9ed7afe5e93845a
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-08-27 14:00:31 +0300

    session cookie installation

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
+
 require('./models/User'); 
 // User model shall start before service passport as the user collection shall be completedebfore passport
 require('./services/passport');
@@ -11,6 +14,15 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 dotenv.config();
 
+app.use( 
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    }) 
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 require('./routes/authRoutes')(app)
