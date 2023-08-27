c107b124075870f72c078c6ae4bdf7f7d35985d6
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-08-27 14:18:22 +0300

    complete testing authentication flow

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use(
         keys: [keys.cookieKey]
     }) 
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,11 @@ const router = express.Router();
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google' , {
         scope: ['profile' ,'email']
-    }) )
+    }));
     
-    app.get('/auth/google/callback', passport.authenticate('google') )
+    app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/current_user' , (req , res) => {
+        res.send(req.user)
+    });
 }
