b415436cf9989883eeeb7fdbe74fcde731dcb4bd
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-08-27 17:19:21 +0300

    adjust in prod.js  error

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod .js - figur out what set of credenital to return
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE.CLIENT_SECRET,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
