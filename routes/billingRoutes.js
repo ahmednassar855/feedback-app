bc0db0fb01f26a4d40bacee0699dd9f5491f9cd0
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-09-01 16:42:28 +0300

    complete api billing and add result to user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,15 +1,19 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe' , async ( req , res ) => {
-      const charge =  await stripe.charges.create({
+    app.post('/api/stripe', requireLogin , async (req, res) => {
+        const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for credits',
             source: req.body.id,
         });
 
-        console.log(charge);
-    });   
+        req.user.credits += 5;
+        const user = await req.user.save();
+
+        res.send(user);
+    });
 };
