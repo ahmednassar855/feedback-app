810c8eb4a1888baa95b97f32d6b6e05f986fd250
ahmednassar855 <ahmed.nassar855@gmail.com>
2023-09-02 02:07:46 +0300

    run build for client and started to depolyment

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
 
+if( process.env.NODE_ENV === 'production' ) {
+    // express will serve up production assets
+
+    // like our main.js file,or main.css file
+    app.use(express.static('client/build'))
+    // express will serve up the index.html file
+
+    // if it does not recoginze the route
+    const path = require('path');
+
+    app.get('*' , ( req , res ) => {
+        res.sendFile(path.resolve(__dirname , 'client' ,'dist','assets' , 'index.html'))
+    })
+}
+
 app.get('/', (req, res) => res.send('Hello there!'));
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
